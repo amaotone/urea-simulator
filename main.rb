518a22f3a51359c01521f7d0d17d833e719e0f6a
sakueji <info@sakueji.com>
2015-11-24 02:15:29 +0900

    add some comments

--- main.rb
+++ main.rb
@@ -3,17 +3,25 @@ require "./reactor"
 require "./flasher"
 
 def optimize(x1, x4, x6, old_x7)
+  # mixer
   x2 = (Vector[*x1] + Vector[*x4] + Vector[*x6]).to_a
+
+  # reactor
   reactor = Reactor.new(x2, 200.0)
   x3 = reactor.outlet
+
+  # high pressure flasher
   high_flasher = Flasher.new(reactor.outlet, 133.0)
   high_flasher.optimize_by_recovery_ratios([0.0, 0.8228, 0.0])
   x4 = high_flasher.vapor
   x5 = high_flasher.liquid
+
+  # low pressure flasher
   low_flasher = Flasher.new(x5, 82.3)
   low_flasher.optimize_by_recovery_ratios([0.0, 0.9737, 0.0])
   x6 = low_flasher.vapor
   x7 = low_flasher.liquid
+
   if (x7[E_NH3]-old_x7[E_NH3]).abs < ACCURACY
     {
       pipes: [x1, x2, x3, x4, x5, x6, x7],
@@ -26,13 +34,18 @@ def optimize(x1, x4, x6, old_x7)
   end
 end
 
+# initial condition
 nh3 = 2.051
 co2 = 1.002
 x1 = [nh3-co2*2, co2, 0.0, 0.0]
 x4 = [0.0, 0.0, 0.0, 0.0]
 x6 = [0.0, 0.0, 0.0, 0.0]
 x7 = [0.0, 0.0, 0.0, 0.0]
+
+# optimizing
 result = optimize(x1, x4, x6, x7)
+
+# show result
 result[:pipes].each.with_index(1) do |pipe, index|
   puts "x#{index}: #{pipe.map{|i| i.round(3)}}"
 end
